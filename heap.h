a4cddd0e864014069aad7918a28186341959e002
최진원갤럭시북3\yun72_92xubzr <munjwc25@gmail.com>
2025-11-02 12:17:20 -0800

    initialize project and add input reading skeleton

--- heap.h
+++ heap.h
@@ -1,36 +1,16 @@
-//
-// Created by Manju Muralidharan on 10/19/25.
-//
-
 #ifndef HEAP_H
 #define HEAP_H
-
 #include <iostream>
 using namespace std;
 
+/*
+  Placeholder header.
+  We will fill heap logic in later commits.
+*/
+const int MAX_NODES = 64;
+
 struct MinHeap {
-    int data[64];
-    int size;
-
-    MinHeap() { size = 0; }
-
-    void push(int idx, int weightArr[]) {
-        // TODO: insert index at end of heap, restore order using upheap()
-    }
-
-    int pop(int weightArr[]) {
-        // TODO: remove and return smallest index
-        // Replace root with last element, then call downheap()
-        return -1; // placeholder
-    }
-
-    void upheap(int pos, int weightArr[]) {
-        // TODO: swap child upward while smaller than parent
-    }
-
-    void downheap(int pos, int weightArr[]) {
-        // TODO: swap parent downward while larger than any child
-    }
+    int size; // not used yet
 };
 
 #endif
